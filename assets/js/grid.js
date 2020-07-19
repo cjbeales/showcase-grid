abb48075662dbc0f5baf56c337eb4e0e3f83d662
Connor <connorj.beales@gmail.com>
2020-07-19 16:08:08 +0100

    Initial commit

---
+++ assets/js/grid.js
@@ -0,0 +1,73 @@
+var gridItem = document.querySelectorAll('.grid-item');
+var showcaseItem = document.querySelectorAll('.showcase');
+var filterBtn = document.querySelectorAll('.filter-btn');
+
+function getAttributes(event) {
+  event.preventDefault();
+
+  var thisValue = event.target.getAttribute('data-filter');
+  let thisBtn = event.target;
+
+
+  for (let a = 0; a < gridItem.length; a++) {
+    const gridFilterAttr = gridItem[a].getAttribute('data-filterType');
+
+    if (thisValue == gridFilterAttr) {
+      gridItem[a].classList.add('active');
+    } else if (thisValue !== gridFilterAttr) {
+      gridItem[a].classList.remove('active');
+    }
+  }
+
+  if (thisValue == 'all' && showcaseItem.className !== 'grid-item showcase active') {
+    for (let i = 0; i < showcaseItem.length; i++) {
+      showcaseItem[i].classList.add('active');
+    }
+  }
+
+  if (thisBtn.classList.contains('active')) {} else {
+    for (let c = 0; c < filterBtn.length; c++) {
+      filterBtn[c].classList.remove('active');
+    }
+    thisBtn.classList.add('active');
+  }
+}
+
+for (let b = 0; b < filterBtn.length; b++) {
+  filterBtn[b].addEventListener('click', getAttributes);
+}
+
+// Overlay
+let workOverlay = document.querySelectorAll('.mywork-overlay');
+
+function viewOverlay(event) {
+  event.preventDefault();
+  let thisValue = event.target.getAttribute('data-project');
+  let thisBtn = event.target;
+
+  for (let a = 0; a < workOverlay.length; a++) {
+    const projectFilterAttr = workOverlay[a].getAttribute('data-projectType');
+
+    if (thisValue == projectFilterAttr) {
+      workOverlay[a].classList.toggle('active');
+    }
+  }
+}
+
+for (let c = 0; c < gridItem.length; c++) {
+  gridItem[c].addEventListener('click', viewOverlay);
+}
+
+// Close Overlay
+let closeBtn = document.querySelectorAll('.closebtn');
+
+function closeOverlay() {
+  for (let i = 0; i < workOverlay.length; i++) {
+    workOverlay[i].classList.remove('active')
+  }
+  document.body.style.overflow = 'visible';
+}
+
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].addEventListener('click', closeOverlay);
+}
